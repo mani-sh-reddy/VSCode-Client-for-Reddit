9fc9fef324fa6e331fab8ff48ea198e9fcba5ab1
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-06-03 13:45:30 +0100

    Create CodeWindow.jsx

---
+++ src/Components/CodeWindow.jsx
@@ -0,0 +1,16 @@
+import "./styling.css";
+import TabBar from "./TabBar";
+
+const CodeWindow = () => {
+	return (
+		<>
+			<TabBar />
+			<div className="codeWindow">
+				<p>03242342343</p>
+				<p>O</p>
+			</div>
+		</>
+	);
+};
+
+export default CodeWindow;
