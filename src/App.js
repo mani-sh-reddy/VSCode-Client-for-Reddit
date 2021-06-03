779ddcafcfd14c9cd50dc12b191232c40c877c49
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-06-03 13:45:23 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Container, Row, Col } from "reactstrap";
+import LeftNav from "./Components/LeftNav";
+import BottomBar from "./Components/BottomBar";
+import ExplorerBar from "./Components/ExplorerBar";
+import CodeWindow from "./Components/CodeWindow";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<LeftNav />
+			<BottomBar />
+			<ExplorerBar />
+			<CodeWindow />
+		</>
+	);
 }
 
 export default App;
