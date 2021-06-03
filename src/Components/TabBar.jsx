56941686e352156bf87845573b4d879482b8c278
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-06-03 13:45:40 +0100

    Create TabBar.jsx

---
+++ src/Components/TabBar.jsx
@@ -0,0 +1,16 @@
+import './styling.css';
+
+const TabBar = () => {
+    
+    return (
+    <div className="tabBar">
+    <p>0</p>
+    <p>O</p>
+    <p>O</p>
+    <p>O</p>
+    <p>O</p>
+    </div>
+ );
+ };
+ 
+ export default TabBar;
