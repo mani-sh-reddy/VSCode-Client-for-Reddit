fe301ed6834fa8e1982d590b401b3938de25de66
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-06-03 13:45:32 +0100

    Create ExplorerBar.jsx

---
+++ src/Components/ExplorerBar.jsx
@@ -0,0 +1,12 @@
+import "./styling.css";
+
+const ExplorerBar = () => {
+	return (
+		<div className="explorerBar">
+			<p>03242342343</p>
+			<p>O</p>
+		</div>
+	);
+};
+
+export default ExplorerBar;
