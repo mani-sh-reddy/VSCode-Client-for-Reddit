92dbac9eff79d3b4ff782888ba6903099d089e89
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-06-03 13:45:28 +0100

    Create BottomBar.jsx

---
+++ src/Components/BottomBar.jsx
@@ -0,0 +1,16 @@
+import './styling.css';
+
+const LeftNav = () => {
+    
+    return (
+    <div className="bottomBar">
+    <p>0</p>
+    <p>O</p>
+    <p>O</p>
+    <p>O</p>
+    <p>O</p>
+    </div>
+ );
+ };
+ 
+ export default LeftNav;
