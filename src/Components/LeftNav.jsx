58b93bf52862b55c1aa06e204510d39a4873156d
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-06-04 10:47:07 +0100

    added left nav icons

--- src/Components/LeftNav.jsx
+++ src/Components/LeftNav.jsx
@@ -1,15 +1,31 @@
-import './styling.css';
+import "./styling.css";
+import NavFilesIcon from "../images/vscode-default-icons/files.svg";
+import NavSearchIcon from "../images/vscode-default-icons/search.svg";
+import NavSourceControlIcon from "../images/vscode-default-icons/source-control.svg";
+import NavDebugAltIcon from "../images/vscode-default-icons/debug-alt.svg";
+import NavRemoteExplorerIcon from "../images/vscode-default-icons/remote-explorer.svg";
+import NavExtensionsIcon from "../images/vscode-default-icons/extensions.svg";
+import NavAccountIcon from "../images/vscode-default-icons/account.svg";
+import NavSettingsIcon from "../images/vscode-default-icons/settings-gear.svg";
 
 const LeftNav = () => {
-    
-    return (
-    <div className="leftNav">
-    <p>A</p>
-    <p>B</p>
-    <p>C</p>
-    <p>D</p>
-    </div>
- );
- };
- 
- export default LeftNav;
+	return (
+		<div className="leftNav">
+            {/* <div className="leftNavTopIcons">
+            </div>
+            <div className="leftNavBottomIcons">
+            </div> */}
+			<img className="leftNavIcon" src={NavFilesIcon} alt="NavFilesIcon" />
+			<img className="leftNavIcon" src={NavSearchIcon} alt="NavSearchIcon" />
+			<img className="leftNavIcon" src={NavSourceControlIcon} alt="NavSourceControlIcon" />
+			<img className="leftNavIcon" src={NavDebugAltIcon} alt="NavDebugAltIcon" />
+			<img className="leftNavIcon" src={NavRemoteExplorerIcon} alt="NavRemoteExplorerIcon" />
+			<img className="leftNavIcon" src={NavExtensionsIcon} alt="NavExtensionsIcon" />
+            <div className="flexSeperator"/>
+			<img className="leftNavIcon" src={NavAccountIcon} alt="NavAccountIcon" />
+			<img className="leftNavIcon" src={NavSettingsIcon} alt="NavSettingsIcon" />
+		</div>
+	);
+};
+
+export default LeftNav;
