5cf19e267f9cbffcd142fd888743c194695f1bc4
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-06-13 13:09:57 +0100

    added get and displaying titles from reddit api

--- src/Components/CodeWindow.jsx
+++ src/Components/CodeWindow.jsx
@@ -1,11 +1,36 @@
-import '../stylesheets/codeWindow.css';
+import "../stylesheets/codeWindow.css";
+import Axios from "axios";
+import React, { useState } from "react";
 
 const CodeWindow = () => {
+	const [posts, setPosts] = useState([]);
+
+	const getPopularPosts = () => {
+		Axios.get("https://www.reddit.com/r/popular/hot.json")
+			.then((response) => {
+				const allPosts = response.data.data.children;
+				setPosts(allPosts);
+				allPosts.forEach((object) => {
+					console.log(object.data.title);
+				});
+
+				// console.log(response.data.data.children);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+
 	return (
 		<>
-			
 			<div className="codeWindow">
-				<p>code window placeholder</p>
+				<div className="codeWindowContent">
+					{posts.map((posts) => {
+						return <p>{posts.data.title}</p>;
+					})}
+
+					<button onClick={getPopularPosts}>log the json</button>
+				</div>
 			</div>
 		</>
 	);
