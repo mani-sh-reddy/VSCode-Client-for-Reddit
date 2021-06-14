916a6e227c73235e162cece1d73ea28e8653c870
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-06-14 20:54:35 +0100

    formatted breadcrumb bar

--- src/Components/BreadCrumbBar.jsx
+++ src/Components/BreadCrumbBar.jsx
@@ -1,14 +1,17 @@
-import "../stylesheets/codeWindow.css";
+import "../stylesheets/breadCrumbBar.css";
 
 const BreadCrumbBar = () => {
-    
-    return (
-    <div className="breadCrumbBar">
-    <p>r/</p>
-    <p>popular/</p>
-    <p>new</p>
-    </div>
- );
- };
- 
- export default BreadCrumbBar;
+	return (
+		<div className="breadCrumbBar">
+			<p className="breadcrumbBarText">{"reddit"}</p>
+			<p className="breadcrumbBarText">{"ᐳ"}</p>
+			<p className="breadcrumbBarText">{"popular"}</p>
+			<p className="breadcrumbBarText">{"ᐳ"}</p>
+			<p className="breadcrumbBarText">{"hot"}</p>
+			<p className="breadcrumbBarText">{"ᐳ"}</p>
+			<p className="breadcrumbBarText">{"past 24 hours"}</p>
+		</div>
+	);
+};
+
+export default BreadCrumbBar;
