fb4de3496d5eaf167f92c8d0872b3f17a74c798b
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-06-15 20:37:37 +0100

    updated explorer bar

--- src/Components/ExplorerBar.jsx
+++ src/Components/ExplorerBar.jsx
@@ -1,16 +1,35 @@
 import "../stylesheets/explorerBar.css";
 
 const ExplorerBar = () => {
-	const chevron = () => {
-		return <img className="leftNavIcon" src={require(`../images/chevron-right.svg`).default} alt="arrow right" />;
-	};
-
 	return (
-		<div className="explorerBar">
-			<p className="explorerBarHeading">EXPLORER</p>
-			{chevron}
-			<span className="explorerTopLevel">WORKSPACE (WORKSPACE)</span>
-		</div>
+		<>
+			<div className="explorerBar">
+				<p className="explorerBarHeading">EXPLORER</p>
+				<span className="explorerLevelWorkspace">WORKSPACE (WORKSPACE)</span>
+				<div className="explorerIndentLarge">
+					<p  className="explorerLevel2">.github</p>
+					<p  className="explorerLevel2">node_modules</p>
+					<p  className="explorerLevel2">public</p>
+					<p  className="explorerLevel2">src</p>
+
+					<div className="explorerIndent indentGuide">
+					<p  className="explorerLevel2">Components</p>
+					<p  className="explorerLevel2">images</p>
+					<p  className="explorerLevel2">stylesheets</p>
+					<p  className="explorerLevel2">App.css</p>
+					<p  className="explorerLevel2">App.js</p>
+					<p  className="explorerLevel2">index.css</p>
+					<p  className="explorerLevel2">reportWebVitals.js</p>
+					<p  className="explorerLevel2">setupTests.js</p>
+					</div>
+					<p  className="explorerLevel2">.gitignore</p>
+					<p  className="explorerLevel2">package-lock.json</p>
+					<p  className="explorerLevel2">package.json</p>
+					<p  className="explorerLevel2">README.md</p>
+					<p  className="explorerLevel2">workspace.code-workspace</p>
+				</div>
+			</div>
+		</>
 	);
 };
 
