7ba9ff904b3f620a08d1fe03438a20c91c1b0243
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-06-15 17:17:23 +0100

    added top level components in explorer bar

--- src/Components/ExplorerBar.jsx
+++ src/Components/ExplorerBar.jsx
@@ -1,18 +1,15 @@
 import "../stylesheets/explorerBar.css";
 
 const ExplorerBar = () => {
+	const chevron = () => {
+		return <img className="leftNavIcon" src={require(`../images/chevron-right.svg`).default} alt="arrow right" />;
+	};
+
 	return (
 		<div className="explorerBar">
-			<p>placeholder</p>
-			<p>placeholder</p>
-			<p>placeholder</p>
-			<p>placeholder</p>
-			<p>placeholder</p>
-			<p>placeholder</p>
-			<p>placeholder</p>
-			<p>placeholder</p>
-			<p>placeholder</p>
-
+			<p className="explorerBarHeading">EXPLORER</p>
+			{chevron}
+			<span className="explorerTopLevel">WORKSPACE (WORKSPACE)</span>
 		</div>
 	);
 };
