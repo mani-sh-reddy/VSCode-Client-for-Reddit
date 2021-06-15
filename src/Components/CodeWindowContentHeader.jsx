d914ca7ba3ff79a35d8e8ec53b5f09eb9b42b2f3
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-06-15 16:39:09 +0100

    fixed spacing

--- src/Components/CodeWindowContentHeader.jsx
+++ src/Components/CodeWindowContentHeader.jsx
@@ -8,35 +8,35 @@ const CodeWindowContentHeader = (props) => {
 	return (
 		<>
 			<div>
-					<span className="codeKeyword">class </span>
-					<span className="codeClass">{`${titleCase(props.subredditName)}`}</span>
-					<span className="codeOperator">{" {"}</span>
+				<span className="codeKeyword">class </span>
+				<span className="codeClass">{`${titleCase(props.subredditName)}`}</span>
+				<span className="codeOperator">{" {"}</span>
 
 				<div className="tab1 indentGuide">
-						<span className="codeKeywordLite">{"constructor"}</span>
-						<span className="codeOperator">{"("}</span>
-						<span className="codeInputs">{"sort"}</span>
-						<span className="codeOperator">{","}</span>
-						<span className="codeInputs">{"time_range"}</span>
-						<span className="codeOperator">{")"}</span>
-						<span className="codeOperator">{" {"}</span>
+					<span className="codeKeywordLite">{"constructor"}</span>
+					<span className="codeOperator">{"("}</span>
+					<span className="codeInputs">{"sort"}</span>
+					<span className="codeOperator">{","}</span>
+					<span className="codeInputs">{"time_range"}</span>
+					<span className="codeOperator">{")"}</span>
+					<span className="codeOperator">{" {"}</span>
 
 					<div className="tab1 indentGuide">
-							<span className="codeKeywordLite">{"this"}</span>
-							<span className="codeOperator">{"."}</span>
-							<span className="codeMethodCall">{"sort"}</span>
-							<span className="codeOperator">{" = "}</span>
-							<span className="codeString">{`"${props.subredditSort}"`}</span>
-							<span className="codeOperator">{";"}</span>
-							<br/>
-							<span className="codeKeywordLite">{"this"}</span>
-							<span className="codeOperator">{"."}</span>
-							<span className="codeMethodCall">{"timeRange"}</span>
-							<span className="codeOperator">{" = "}</span>
-							<span className="codeString">{`"${props.subredditSortTimeRange}"`}</span>
-							<span className="codeOperator">{";"}</span>
+						<span className="codeKeywordLite">{"this"}</span>
+						<span className="codeOperator">{"."}</span>
+						<span className="codeMethodCall">{"sort"}</span>
+						<span className="codeOperator">{" = "}</span>
+						<span className="codeString">{`"${props.subredditSort}"`}</span>
+						<span className="codeOperator">{";"}</span>
+						<br />
+						<span className="codeKeywordLite">{"this"}</span>
+						<span className="codeOperator">{"."}</span>
+						<span className="codeMethodCall">{"timeRange"}</span>
+						<span className="codeOperator">{" = "}</span>
+						<span className="codeString">{`"${props.subredditSortTimeRange}"`}</span>
+						<span className="codeOperator">{";"}</span>
 					</div>
-						<span className="codeOperator">{"}"}</span>
+					<span className="codeOperator">{"}"}</span>
 					<br />
 				</div>
 			</div>
